b043d156f9c12ddcef4651c99e650716b0daae01
Eric Bachhuber <bachhuberdesign@gmail.com>
2017-08-15 08:02:10 -0500

    #1 Adding in Moshi serializer

---
+++ library/src/main/java/io/milkcan/effortlessprefs/library/MoshiSerializer.kt
@@ -0,0 +1,41 @@
+package io.milkcan.effortlessprefs.library
+
+import android.content.SharedPreferences
+import com.squareup.moshi.Moshi
+
+/**
+ * @author Eric Bachhuber
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+class MoshiSerializer(val moshi: Moshi) : PrefSerializer {
+
+    lateinit var prefs: SharedPreferences
+
+    override fun setSharedPreferenceInstance(sharedPreferences: SharedPreferences) {
+        prefs = sharedPreferences
+    }
+
+    override fun putObject(key: String, value: Any) {
+        val adapter = moshi.adapter<Any>(value::class.java)
+        val json = adapter.toJson(value)
+
+        prefs.edit().putString(key, json).apply()
+    }
+
+    override fun <T : Any> getObject(key: String, defaultValue: T): T {
+        val adapter = moshi.adapter<T>(defaultValue::class.java)
+        val json = prefs.getString(key, "")
+
+        return if (json.isNullOrBlank()) {
+            defaultValue
+        } else {
+            adapter.fromJson(json) as T
+        }
+    }
+
+    override fun <T : Any> getObject(key: String): T? {
+        TODO("Not yet implemented.")
+    }
+
+}
